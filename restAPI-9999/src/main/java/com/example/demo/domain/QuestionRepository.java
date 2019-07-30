736bd2b4d57708469854437bc1474e351908d226
Administrator <Administrator@SKCC14D00683.SKCC.NET>
2019-07-30 11:31:54 +0900

    질문 상세

--- restAPI-9999/src/main/java/com/example/demo/domain/QuestionRepository.java
+++ restAPI-9999/src/main/java/com/example/demo/domain/QuestionRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 //vo, id의 타입
 public interface QuestionRepository extends JpaRepository<Question,Long>{
-
+	Optional<Question> findById(Long id);
 }
--- restAPI-9999/src/main/java/com/example/demo/web/QuestionController.java
+++ restAPI-9999/src/main/java/com/example/demo/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,7 +33,6 @@ public class QuestionController {
 		if(!HttpSessionUtil.isLoginUser(session)) {
 			return "/users/loginForm";
 		}
-		
 		return "/qna/form";
 	}
 	
@@ -51,5 +52,20 @@ public class QuestionController {
 		return "redirect:/";
 	}
 	
+	
+	
+	//상세보기
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		
+		Question qna = questionRepository.findById(id).get();
+		
+		
+		System.out.println("qna : " + qna);
+		
+		model.addAttribute("question",qna );
+		
+		return "/qna/show";
+	}
 	
 }
